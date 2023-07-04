05fcf03bbd41daebdbe3fed87daf134a1cf1ee5a
Hashini <hashini.liyanage@gmail.com>
2023-07-04 20:18:49 +0200

    Todo List View and Program Exit #match-case_1

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ todos = []
 while True:
     user_action = input("add or show")
     match user_action:
-        case 'show':
-            print(todos)
         case 'add':
             user_prompt = "Enter a todo:"
             todo = input(user_prompt)
             todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break:
 
 
