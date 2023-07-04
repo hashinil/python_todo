be3528f7aeb954bcac38bb3cb594f27b82ae1f6a
Hashini <hashini.liyanage@gmail.com>
2023-07-04 20:27:54 +0200

    Improving the Program Output #for-loops

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 todos = []
 while True:
-    user_action = input("add , show or exit")
+    user_action = input("add , show or exit: ")
+    user_action = user_action.strip()
     match user_action:
         case 'add':
             user_prompt = "Enter a todo:"
             todo = input(user_prompt)
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
         case 'exit':
             break
 
