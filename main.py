c373567d13047e7c6f5c348830653e713387097c
Hashini <hashini.liyanage@gmail.com>
2023-07-05 09:43:49 +0200

    Add an Edit Feature #type-conversion #list-indexing

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 todos = []
 while True:
-    user_action = input("add , show or exit: ")
+    user_action = input("add , edit, show or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            user_prompt = "Enter a todo:"
+            user_prompt = "Enter a todo: "
             todo = input(user_prompt)
             todos.append(todo)
+        case 'edit':
+            user_index = int(input("what item need to edit: "))
+            user_index = user_index - 1
+            new_todo = input("Enter a new todo: ")
+            todos[user_index] = new_todo
         case 'show':
             for item in todos:
                 print(item)
