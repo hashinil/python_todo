e12114477319ad4de95d3a58a59692b0e4c2459a
Hashini <hashini.liyanage@gmail.com>
2023-07-07 15:17:11 +0200

    Numbered Todos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 todos = []
 while True:
-    user_action = input("add , edit, show or exit: ")
+    user_action = input("add , edit, show, complete or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
@@ -14,7 +14,12 @@ while True:
             todos[user_index] = new_todo
         case 'show':
             for index, item in enumerate(todos):
-                print(index, '-', item)
+                row = f"{index+1}-{item}"
+                print(row)
+        case 'complete':
+            user_index = int(input("what item need to complete: "))
+            user_index = user_index - 1
+            todos.pop(user_index)
         case 'exit':
             break
 
