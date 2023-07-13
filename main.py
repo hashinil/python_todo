648eda999dd5b20295ac8346f54ad5f1fb6a402d
Hashini <hashini.liyanage@gmail.com>
2023-07-13 22:20:20 +0200

    Optimising the Code #with-context-manager-1

--- main.py
+++ main.py
@@ -3,25 +3,23 @@ while True:
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            file_r = open('todo.txt', 'r')
-            todos = file_r.readlines()
-            file_r.close()
+            with open('todo.txt', 'r') as file_r:
+                todos = file_r.readlines()
 
             user_prompt = "Enter a todo: "
             todo = input(user_prompt)+'\n'
             todos.append(todo)
 
-            file_w = open('todo.txt', 'w')
-            file_w.writelines(todos)
-            file_w.close()
+            with open('todo.txt', 'w') as file_w:
+                file_w.writelines(todos)
         case 'edit':
             user_index = int(input("what item need to edit: "))
             user_index = user_index - 1
             new_todo = input("Enter a new todo: ")
             todos[user_index] = new_todo
         case 'show':
-            file_s = open('todo.txt', 'r')
-            todos = file_s.readlines()
+            with open('todo.txt', 'r') as file_s:
+                todos = file_s.readlines()
 
             for index, item in enumerate(todos):
                 item = item.strip('\n')
