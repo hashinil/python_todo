931b98b4d6dc176782e52f5b61066c746e01658f
Hashini <hashini.liyanage@gmail.com>
2023-07-17 12:49:28 +0200

    Improving - #if-conditionals #slicing

--- main.py
+++ main.py
@@ -1,52 +1,54 @@
 while True:
     user_action = input("add , edit, show, complete or exit: ")
     user_action = user_action.strip()
-    match user_action:
-        case 'add':
-            with open('todo.txt', 'r') as file_r:
-                todos = file_r.readlines()
 
-            user_prompt = "Enter a todo: "
-            todo = input(user_prompt)+'\n'
-            todos.append(todo)
+    if 'add' in user_action:
+        with open('todo.txt', 'r') as file_r:
+            todos = file_r.readlines()
 
-            with open('todo.txt', 'w') as file_w:
-                file_w.writelines(todos)
-        case 'edit':
-            user_index = int(input("what item need to edit: "))
-            user_index = user_index - 1
+        todo = user_action[4:]+'\n'
+        todos.append(todo)
 
-            with open('todo.txt', 'r') as file_r:
-                todos = file_r.readlines()
+        with open('todo.txt', 'w') as file_w:
+            file_w.writelines(todos)
+    elif 'edit' in user_action:
+        user_index = int(user_action[5:])
+        user_index = user_index - 1
 
-            new_todo = input("Enter a new todo: ")
-            todos[user_index] = new_todo+'\n'
+        with open('todo.txt', 'r') as file_r:
+            todos = file_r.readlines()
 
-            with open('todo.txt', 'w') as file_w:
-                file_w.writelines(todos)
-        case 'show':
-            with open('todo.txt', 'r') as file_s:
-                todos = file_s.readlines()
+        new_todo = input("Enter a new todo: ")
+        todos[user_index] = new_todo+'\n'
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index+1}-{item}"
-                print(row)
-        case 'complete':
-            user_index = int(input("what item need to complete: "))
-            user_index = user_index - 1
+        with open('todo.txt', 'w') as file_w:
+            file_w.writelines(todos)
+    elif 'show' in user_action:
+        with open('todo.txt', 'r') as file_s:
+            todos = file_s.readlines()
 
-            with open('todo.txt', 'r') as file_r:
-                todos = file_r.readlines()
-            todo_to_remove = todos[user_index]
-            todos.pop(user_index)
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index+1}-{item}"
+            print(row)
+    elif 'complete' in user_action:
+        user_index = int(user_action[9:])
+        user_index = user_index - 1
 
-            with open('todo.txt', 'w') as file_w:
-                file_w.writelines(todos)
-            message = f"removed: {todo_to_remove}"
-            print(message)
-        case 'exit':
-            break
+        with open('todo.txt', 'r') as file_r:
+            todos = file_r.readlines()
+        todo_to_remove = todos[user_index]
+        todos.pop(user_index)
+
+        with open('todo.txt', 'w') as file_w:
+            file_w.writelines(todos)
+        message = f"removed: {todo_to_remove}"
+        print(message)
+    elif 'exit' in user_action:
+        break
+    else:
+        print("Invalid command")
+
 
 print('bye')
 
