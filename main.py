d532287b803caa461fe4a589721edf278964ea52
Hashini <hashini.liyanage@gmail.com>
2023-07-17 19:34:23 +0200

    try-except #continue

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ while True:
     user_action = input("add , edit, show, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if user_action.startswith("add"):
         with open('todo.txt', 'r') as file_r:
             todos = file_r.readlines()
 
@@ -11,19 +11,23 @@ while True:
 
         with open('todo.txt', 'w') as file_w:
             file_w.writelines(todos)
-    elif 'edit' in user_action:
-        user_index = int(user_action[5:])
-        user_index = user_index - 1
+    elif user_action.startswith("edit"):
+        try:
+            user_index = int(user_action[5:])
+            user_index = user_index - 1
 
-        with open('todo.txt', 'r') as file_r:
-            todos = file_r.readlines()
+            with open('todo.txt', 'r') as file_r:
+                todos = file_r.readlines()
 
-        new_todo = input("Enter a new todo: ")
-        todos[user_index] = new_todo+'\n'
+            new_todo = input("Enter a new todo: ")
+            todos[user_index] = new_todo+'\n'
 
-        with open('todo.txt', 'w') as file_w:
-            file_w.writelines(todos)
-    elif 'show' in user_action:
+            with open('todo.txt', 'w') as file_w:
+                file_w.writelines(todos)
+        except ValueError:
+            print("Your command is invalid!")
+            continue
+    elif user_action.startswith("show"):
         with open('todo.txt', 'r') as file_s:
             todos = file_s.readlines()
 
@@ -31,20 +35,24 @@ while True:
             item = item.strip('\n')
             row = f"{index+1}-{item}"
             print(row)
-    elif 'complete' in user_action:
-        user_index = int(user_action[9:])
-        user_index = user_index - 1
+    elif user_action.startswith("complete"):
+        try:
+            user_index = int(user_action[9:])
+            user_index = user_index - 1
 
-        with open('todo.txt', 'r') as file_r:
-            todos = file_r.readlines()
-        todo_to_remove = todos[user_index]
-        todos.pop(user_index)
+            with open('todo.txt', 'r') as file_r:
+                todos = file_r.readlines()
+            todo_to_remove = todos[user_index]
+            todos.pop(user_index)
 
-        with open('todo.txt', 'w') as file_w:
-            file_w.writelines(todos)
-        message = f"removed: {todo_to_remove}"
-        print(message)
-    elif 'exit' in user_action:
+            with open('todo.txt', 'w') as file_w:
+                file_w.writelines(todos)
+            message = f"removed: {todo_to_remove}"
+            print(message)
+        except IndexError:
+            print("No Item")
+            continue
+    elif user_action.startswith("exit"):
         break
     else:
         print("Invalid command")
