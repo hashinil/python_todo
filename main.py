ebbca250e166d122df7eddd29d24d2802713b46b
Hashini <hashini.liyanage@gmail.com>
2023-07-18 11:19:00 +0200

    #function-arguments

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
-def get_todos():
-    with open('todo.txt', 'r') as file_read:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_read:
         todos_local = file_read.readlines()
     return todos_local
+
+
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file_w:
+        file_w.writelines(todos_arg)
+
 
 while True:
     user_action = input("add , edit, show, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith("add"):
-        todos = get_todos()
+        todos = get_todos('todo.txt')
 
         todo = user_action[4:]+'\n'
         todos.append(todo)
 
-        with open('todo.txt', 'w') as file_w:
-            file_w.writelines(todos)
+        write_todos('todo.txt', todos)
     elif user_action.startswith("edit"):
         try:
             user_index = int(user_action[5:])
             user_index = user_index - 1
 
-            todos = get_todos()
+            todos = get_todos('todo.txt')
 
             new_todo = input("Enter a new todo: ")
             todos[user_index] = new_todo+'\n'
 
-            with open('todo.txt', 'w') as file_w:
-                file_w.writelines(todos)
+            write_todos('todo.txt', todos)
         except ValueError:
             print("Your command is invalid!")
             continue
     elif user_action.startswith("show"):
-        todos = get_todos()
+        todos = get_todos('todo.txt')
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -42,12 +46,11 @@ while True:
             user_index = int(user_action[9:])
             user_index = user_index - 1
 
-            todos = get_todos()
+            todos = get_todos('todo.txt')
             todo_to_remove = todos[user_index]
             todos.pop(user_index)
 
-            with open('todo.txt', 'w') as file_w:
-                file_w.writelines(todos)
+            write_todos('todo.txt', todos)
             message = f"removed: {todo_to_remove}"
             print(message)
         except IndexError:
