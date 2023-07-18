cc28b03f99a4bbf349ca203a75ee23f275e34cd8
Hashini <hashini.liyanage@gmail.com>
2023-07-18 15:53:48 +0200

    Add a Date Feature #standard-modules #time #constant

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
-def get_todos(filepath='todo.txt'):
+FILEPATH = 'todo.txt'
+
+
+def get_todos(filepath=FILEPATH):
     """ Read Todos """
     with open(filepath, 'r') as file_read:
         todos_local = file_read.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath='todo.txt'):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write Todos """
     with open(filepath, 'w') as file_w:
         file_w.writelines(todos_arg)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #from functions import get_todos,write_todos
 import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
 while True:
     user_action = input("add , edit, show, complete or exit: ")
     user_action = user_action.strip()
