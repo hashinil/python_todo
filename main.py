43167766bda5025da96712d6a2f01cdf57352990
Hashini <hashini.liyanage@gmail.com>
2023-07-18 11:37:37 +0200

    #default-arguments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def get_todos(filepath):
+def get_todos(filepath='todo.txt'):
     with open(filepath, 'r') as file_read:
         todos_local = file_read.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath='todo.txt'):
     with open(filepath, 'w') as file_w:
         file_w.writelines(todos_arg)
 
@@ -14,28 +14,28 @@ while True:
     user_action = user_action.strip()
 
     if user_action.startswith("add"):
-        todos = get_todos('todo.txt')
+        todos = get_todos()
 
         todo = user_action[4:]+'\n'
         todos.append(todo)
 
-        write_todos('todo.txt', todos)
+        write_todos(todos)
     elif user_action.startswith("edit"):
         try:
             user_index = int(user_action[5:])
             user_index = user_index - 1
 
-            todos = get_todos('todo.txt')
+            todos = get_todos()
 
             new_todo = input("Enter a new todo: ")
             todos[user_index] = new_todo+'\n'
 
-            write_todos('todo.txt', todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is invalid!")
             continue
     elif user_action.startswith("show"):
-        todos = get_todos('todo.txt')
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -46,11 +46,11 @@ while True:
             user_index = int(user_action[9:])
             user_index = user_index - 1
 
-            todos = get_todos('todo.txt')
+            todos = get_todos()
             todo_to_remove = todos[user_index]
             todos.pop(user_index)
 
-            write_todos('todo.txt', todos)
+            write_todos(todos)
             message = f"removed: {todo_to_remove}"
             print(message)
         except IndexError:
