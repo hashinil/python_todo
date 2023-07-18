042bb1ed0c2104616b30816051859e4455ec707e
Hashini <hashini.liyanage@gmail.com>
2023-07-18 11:38:53 +0200

    #doc-strings

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_todos(filepath='todo.txt'):
+    """ Read Todos """
     with open(filepath, 'r') as file_read:
         todos_local = file_read.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath='todo.txt'):
+    """ Write Todos """
     with open(filepath, 'w') as file_w:
         file_w.writelines(todos_arg)
 
