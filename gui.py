a1bb9743af3e23fd950d7e6dbfdb7bca48eff3cb
Hashini <hashini.liyanage@gmail.com>
2023-07-20 19:36:56 +0200

    Implemented Edit

--- gui.py
+++ gui.py
@@ -2,15 +2,17 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo_input")
 add_btn = sg.Button("Add")
-#edit_btn = sg.Button("Edit")
+
+list_box = sg.Listbox(values=functions.get_todos(), key="todos_list_box", enable_events=True, size=(45, 10))
+edit_btn = sg.Button("Edit")
 #comp_btn = sg.Button("Complete")
 #exit_btn = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_btn]],
-                   font=('Helvetica', 20))
+                   layout=[[label], [input_box, add_btn], [list_box, edit_btn]],
+                   font=('Helvetica', 14))
 
 while True:
     event, value = window.read()
@@ -20,9 +22,21 @@ while True:
     match event:
         case 'Add':
             todos = functions.get_todos()
-            new_todo = value['todo'] + '\n'
+            new_todo = value['todo_input'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos_list_box'].update(values=todos)
+        case 'Edit':
+            edit_todo = value['todos_list_box'][0]
+            new_todo = value['todo_input']
+
+            todos = functions.get_todos()
+            index = todos.index(edit_todo)
+            todos[index] = new_todo + '\n'
+            functions.write_todos(todos)
+            window['todos_list_box'].update(values=todos)
+        case 'todos_list_box':
+            window['todo_input'].update(value=value['todos_list_box'][0])
         case sg.WINDOW_CLOSED:
             break
 
