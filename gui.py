fe979a068d06badd3059b5a98349ca909e04e4b1
Hashini <hashini.liyanage@gmail.com>
2023-07-24 21:29:51 +0200

    creating a standalone executable

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todo.txt'):
+    with open('todo.txt', 'w') as file:
+        pass
 
 sg.theme("Black")
 clock_lbl =sg.Text('', key='clock')
